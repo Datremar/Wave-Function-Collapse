0edad9c6b36298f1c40a0cb2916bc0cc75f74684
Datremar <mr.protoss@bk.ru>
2022-07-22 20:27:57 +0700

    Created screen

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import pygame
+
+WIDTH = 500
+HEIGHT = 500
+
 
 if __name__ == '__main__':
-    print("Hello, World!")
+    pygame.init()
+
+    screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    pygame.display.set_caption("WFC")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(color=(0, 0, 0))
+
+        pygame.display.flip()
