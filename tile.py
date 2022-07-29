5602470ef72c7037a518caff797934351a636157
Datremar <mr.protoss@bk.ru>
2022-07-29 23:17:58 +0700

    Created A Tile class to represent a cell with entropy

--- tile.py
+++ tile.py
@@ -14,18 +14,43 @@ class TileSprite:
     def set_pos(self, x, y):
         self.x = x
         self.y = y
-        print(self)
+
+    def set_image(self, image_path):
+        self.image = pygame.image.load(image_path)
 
     def draw(self, screen):
-        # print(self)
         screen.blit(self.image, self.image.get_rect().move(self.x, self.y))
 
     def __repr__(self):
         return f"{self.x}, {self.y}"
 
 
-if __name__ == "__main__":
-    tile = TileSprite()
-    print(tile)
-    tile.set_pos(1, 1)
-    print(tile)
+class Tile:
+    def __init__(self):
+        self.sprite = TileSprite()
+        self.variants = [x for x in range(5)]
+        self.entropy = len(self.variants)
+
+        self.sprite.set_image("resources/images/uncertain.png")
+
+    def collapse(self, *variants_to_exclude):
+        for exclusion in variants_to_exclude:
+            self.variants.remove(exclusion)
+
+        self.entropy = len(self.variants)
+
+        if self.entropy == 1:
+            self.sprite.set_image("resources/images/Tile1.png")
+        elif self.entropy > 0:
+            self.sprite.set_image("resources/images/uncertain.png")
+        else:
+            self.sprite.set_image("resources/images/TestTile.png")
+
+    def set_pos(self, x, y):
+        self.sprite.set_pos(x, y)
+
+    def draw(self, screen):
+        self.sprite.draw(screen)
+
+    def __repr__(self):
+        return f"{self.entropy} {self.variants} {self.sprite.__repr__()}"
