72c9f2fb88850e134b4905ced35d08daa800e60e
Datremar <mr.protoss@bk.ru>
2022-07-31 12:01:05 +0700

    a few tweaks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from grid import TileGrid
 
+
 WIDTH = 510
 HEIGHT = 510
 
@@ -15,8 +16,6 @@ if __name__ == '__main__':
     pygame.display.set_caption("WFC")
 
     grid = TileGrid()
-    grid.tiles[1][1].collapse(*[1, 2, 3, 4])
-    # grid.json_to_grid("json_data/grid_prototype.json")
 
     running = True
     while running:
@@ -30,4 +29,3 @@ if __name__ == '__main__':
 
         pygame.display.flip()
 
-    print(grid)
